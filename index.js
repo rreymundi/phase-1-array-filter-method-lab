a690c5bf7864d54b5fee4805f90f18c525f0ca57
raul reymundi <kinskispiral@gmail.com>
2022-05-14 18:33:54 -0400

    commit tree 670826ae1cc18702219bc5ac35a6edd83d261416

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution here
+
+// #1
+const findMatching = (array, string) => array.filter(element => element.toLowerCase() === string.toLowerCase());
+// #2
+const fuzzyMatch = (array, string) => array.filter(element => element[0] === string[0]);
+// #3
+const matchName = (array, string) => array.filter(element => element.name === string);
